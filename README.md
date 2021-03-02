6400b7835262edde038da63bf7cf55ec4630e2ee
brianp702 <jiggapayne2@hotmail.com>
2021-03-02 01:24:20 -0800

    Added caching in MongoDB to reduce api calls

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Frontend for mourits-lyrics song lyrics api.
 
 In a web browser, navigate to /lyrics/[artist]/[song name] and the lyrics of the song will display.
+
+Added caching in MongoDB to reduce calls to the api.
--- app.js
+++ app.js
@@ -1,46 +1,80 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 require('dotenv').config();
 
-app.get('/lyrics/:artist/:song', function(req, res) {
-    const artist = req.params.artist;
-    const song = req.params.song;
-
-    var axios = require("axios").default;
-
-    var options = {
-        method: 'GET',
-        url: 'https://mourits-lyrics.p.rapidapi.com/',
-        params: { artist: artist, song: song },
-        headers: {
-            'x-rapidapi-key': process.env.XRAPIDAPIKEY,
-            'x-rapidapi-host': 'mourits-lyrics.p.rapidapi.com'
-        }
+app.get('/lyrics/:artist/:song', (req, res) => {
+    const artistAndSong = {
+        artist: req.params.artist,
+        song: req.params.song
     };
 
-    /* response format
-    {
-        "success": true,
-        "result": {
-            "lyrics": "Tommy used to work on the docks...",
-            "source": {
-                "name": "SongMeanings",
-                "homepage": "https://songmeanings.com/",
-                "url": "https://songmeanings.com/songs/view/11236/"
-            }
-        },
-        "artist": "Bon Jovi",
-        "song": "Livin' On A Prayer"
-    } 
-    */
+    // check mongoDB: did we already cache that song?
+    const MongoClient = require('mongodb').MongoClient;
 
-    axios.request(options).then(function(response) {
-        console.log(response.data.result.lyrics);
-        res.send(response.data.result.lyrics);
-    }).catch(function(error) {
-        console.error(error);
-        res.send("error connecting to the lyrics api");
-    });
+    MongoClient.connect(process.env.MONGOCONNECTIONSTRING, { useUnifiedTopology: true })
+        .then(client => {
+            console.log('connected to db');
+            const db = client.db('myFirstDatabase');
+
+            db.collection('song-lyrics').findOne(artistAndSong)
+                .then(results => {
+
+                    console.log(results);
+
+                    if (results) {
+                        console.log('the song is in the db');
+                    } else {
+                        console.log('the song is not in the db. Get lyrics from mourits-lyrics api');
+
+                        const axios = require("axios").default;
+                        const options = {
+                            method: 'GET',
+                            url: 'https://mourits-lyrics.p.rapidapi.com/',
+                            params: artistAndSong,
+                            headers: {
+                                'x-rapidapi-key': process.env.XRAPIDAPIKEY,
+                                'x-rapidapi-host': 'mourits-lyrics.p.rapidapi.com'
+                            }
+                        };
+
+                        /* response format
+                        {
+                            "success": true,
+                            "result": {
+                                "lyrics": "Tommy used to work on the docks...",
+                                "source": {
+                                    "name": "SongMeanings",
+                                    "homepage": "https://songmeanings.com/",
+                                    "url": "https://songmeanings.com/songs/view/11236/"
+                                }
+                            },
+                            "artist": "Bon Jovi",
+                            "song": "Livin' On A Prayer"
+                        } 
+                        */
+
+                        axios.request(options)
+                            .then(function(response) {
+                                console.log(response.data.result.lyrics);
+                                artistAndSong.lyrics = response.data.result.lyrics;
+
+                                db.collection('song-lyrics').insertOne(artistAndSong)
+                                    .then(result => {
+                                        console.log('song was added to db');
+                                    })
+                                    .catch(error => console.error(error));
+
+                                res.send(response.data.result.lyrics);
+                            }).catch(function(error) {
+                                console.error(error);
+                                res.send("error connecting to the lyrics api");
+                            });
+                    }
+                })
+                .catch(error => console.error(error));
+
+        })
+        .catch(console.error);
 });
 
 app.listen(3000, () => {
